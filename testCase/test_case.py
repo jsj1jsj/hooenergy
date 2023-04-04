38510cd68e96f6cb569c7f187da2a22664d53a37
jsj1jsj <441517994@qq.com>
2023-04-04 09:38:36 +0800

    config

--- testCase/test_case.py
+++ testCase/test_case.py
@@ -173,11 +173,7 @@ class TestCase(object):
 
 if __name__ == '__main__':
     # pytest.main()
-    # testData = GetData().get_excel_data()[244:247] #02 小程序
-    # testData = GetData().get_excel_data()[257:307] #02 美团
-    # testData = GetData().get_excel_data()[306:307] #03 飞猪
-    # testData = GetData().get_excel_data()[309:379]  # 03 分销商
-    testData = GetData().get_excel_data()[382:384]  # 03 同程
+    testData = GetData().get_excel_data()[2:3]  # 01充电
     TestCase().setup_class()
     for i in range(0, 2):
         TestCase().test_case(testData[i])
